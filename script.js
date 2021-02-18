a638822bac30a427129eafbd900cfe412e9f5b3d
Daisy Murillo <daisy21murillo@gmail.com>
2021-02-17 22:11:09 -0700

    functionality to add: done

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let input = document.getElementById("input");
-let enter = docuemnt.getElementById("enter");
+let enter = document.getElementById("enter");
 let ul = document.querySelector("ul");
+let li = document.getElementById("li");
+
+
+function createItem() {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+
+enter.addEventListener("click", createItem);
